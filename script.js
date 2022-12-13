e46ca644370efa0781ea2137ee4c6267c06ec660
mcdmat <[matheyd@gmail.com](mailto:matheyd@gmail.com)>
2022-12-13 21:23:35 +0100

    resolve errors of W3C validator

--- script.js
+++ script.js
@@ -19,22 +19,3 @@ prevButton.addEventListener("click", () => {
 
 
 
-// script for press page
-
-// const modal = document.querySelector("#myModal");
-// let press = document.querySelectorAll(".pressImg");
-// const bigPress = document.querySelector(".modal-content img");
-
-// press = [...press];
-
-// for (let i = 0; i < press.length; i++) {
-//   press[i].addEventListener("click", () => {
-//     let imgSrc = press[i].src.split("/").pop();
-//     bigPress.src = `./press/maxi_${imgSrc}`;
-//     modal.style.display = "block";
-//   });
-// }
-
-// modal.addEventListener("click", () => {
-//   modal.style.display = "none";
-// });
