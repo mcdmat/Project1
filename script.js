4234f40dc7230fa0e1bebc7c29e6c76a6ce8d17f
mcdmat <[matheyd@gmail.com](mailto:matheyd@gmail.com)>
2022-12-13 21:01:20 +0100

    enlarge the menu on the index page and modification of the font import mode

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
   
 const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
+
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 
