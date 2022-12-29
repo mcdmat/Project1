b1a94f81ca7a10219b6d6ca558965f63dd9aaca5
mcdmat <matheyd@gmail.com>
2022-12-29 22:41:19 +0100

    random text with click on logo

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 // script for index page
   
 const slidesContainer = document.getElementById("slides-container");
-const slide = document.querySelector(".slide");
-
-const prevButton = document.getElementById("slide-arrow-prev");
-const nextButton = document.getElementById("slide-arrow-next");
-
-nextButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft += slideWidth;
-});
-
-prevButton.addEventListener("click", () => {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
-});
+const slideButton = document.querySelector(".logo_index");
 
 
 
+const text = [
+  "Aux échecs, l'amour propre est satisfaisant quand on remporte la victoire,</br> mais il n'y a pas de honte à être battu.</br>(Benjamin Franklin)",
+  "Il vaut mieux avoir un plan incorrect que de ne pas avoir de plan du tout.</br> (Victor Kortchnoï)",
+  "On n'a jamais gagné une partie en abandonnant.</br>(Savielly Tartacover)",
+  "Les sacrifices démontrent seulement que quelqu'un a gaffé.</br>(Savielly Tartacover)",
+  "La menace est plus forte que son exécution.</br>(Aaron Nimzovitch)",
+];
+slideButton.addEventListener("click", () => {
+const slide = (array) => {
+  let i = Math.floor(Math.random() * array.length);
+    slidesContainer.innerHTML = `<p> ${array[i]} </p>`;
+ }
+ slide(text);
+  });
 
